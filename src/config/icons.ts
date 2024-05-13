9873e42b5724748298b082c41adbd743c9c336ee
Thomas Wilkinson <twilkinson3421@outlook.com>
2024-05-13 14:49:14 +0100

    Icon Configuration

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -1,5 +1,7 @@
 import { ForwardRefExoticComponent, RefAttributes } from "react";
 
+import { IconMenu, IconUser } from "@tabler/icons-react";
+
 import type { Icon, IconProps } from "@tabler/icons-react";
 
 export namespace Icons {
@@ -9,4 +11,27 @@ export namespace Icons {
       Omit<Icon, "ref"> & RefAttributes<Icon>
     >;
   }
+
+  export namespace Internal {
+    export type Declaration = Record<
+      string,
+      Record<string, Icons.Types.Component>
+    >;
+
+    export function initIcons<IconsDeclaration extends Declaration>(
+      icons: IconsDeclaration
+    ) {
+      return icons;
+    }
+  }
+
+  export const { Interface, Application } = Icons.Internal.initIcons({
+    Interface: {
+      Menu: IconMenu,
+    },
+
+    Application: {
+      User: IconUser,
+    },
+  });
 }
