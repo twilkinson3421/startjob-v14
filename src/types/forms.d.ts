fbd709f1226dfc55acdc1be5fc40576d71cfad9b
Thomas Wilkinson <twilkinson3421@outlook.com>
2024-05-13 13:52:44 +0100

    Separation of Frontend & Backend Types

--- src/types/forms.d.ts
+++ src/types/forms.d.ts
@@ -3,13 +3,22 @@ import { UserModel } from "@/models/user";
 declare namespace FormTypes {
   export namespace Basic {}
 
-  export namespace Expect {
+  export namespace Fields {
     export type Register = {
       firstName: UserModel["firstName"];
       lastName: UserModel["lastName"];
       email: UserModel["email"];
       password: string;
       confirmPassword: string;
+    };
+  }
+
+  export namespace Expect {
+    export type Register = {
+      firstName: UserModel["firstName"];
+      lastName: UserModel["lastName"];
+      email: UserModel["email"];
+      password: string;
     };
   }
 }
