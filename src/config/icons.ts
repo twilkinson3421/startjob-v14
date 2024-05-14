ff2435c4105d750067c35edb846a40451f937ff1
Thomas Wilkinson <twilkinson3421@outlook.com>
2024-05-14 13:44:48 +0100

    Icon Configuration Update

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -1,15 +1,11 @@
-import { ForwardRefExoticComponent, RefAttributes } from "react";
+import { Equals, User, Users } from "@phosphor-icons/react/dist/ssr";
 
-import { IconMenu, IconUser } from "@tabler/icons-react";
-
-import type { Icon, IconProps } from "@tabler/icons-react";
+import type { Icon, IconProps } from "@phosphor-icons/react";
 
 export namespace Icons {
   export namespace Types {
     export type ComponentProps = IconProps;
-    export type Component = ForwardRefExoticComponent<
-      Omit<Icon, "ref"> & RefAttributes<Icon>
-    >;
+    export type Component = Icon;
   }
 
   export namespace Internal {
@@ -18,20 +14,31 @@ export namespace Icons {
       Record<string, Icons.Types.Component>
     >;
 
-    export function initIcons<IconsDeclaration extends Declaration>(
+    export const initIcons = <IconsDeclaration extends Declaration>(
       icons: IconsDeclaration
-    ) {
-      return icons;
-    }
+    ) => icons;
   }
+
+  export const Sizes = {
+    default: 16,
+    smallx: 12,
+    small: 14,
+    medium: 22,
+    large: 28,
+    largex: 32,
+    large2x: 48,
+    large3x: 64,
+    inline: undefined,
+  } as const satisfies Record<string, IconProps["size"]>;
 
   export const { Interface, Application } = Icons.Internal.initIcons({
     Interface: {
-      Menu: IconMenu,
+      Menu: Equals,
     },
 
     Application: {
-      User: IconUser,
+      User: User,
+      Team: Users,
     },
   });
 }
