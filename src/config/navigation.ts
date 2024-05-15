9652e76202290c56f90b271e826ba0e63c6448f0
Thomas Wilkinson <twilkinson3421@outlook.com>
2024-05-15 13:48:50 +0100

    Type Import Updates

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,30 +1,34 @@
-import { ReplaceStringPart } from "@/types/utils";
-
-export namespace navigation {
-  export const routes = {
+export namespace Navigation {
+  export const Routes = {
     home: "/",
     login: "/login",
     register: "/register",
   } as const;
 
-  export const external = {
+  export const External = {
     googleMaps: "https://maps.google.com/?q={location}",
   } as const;
 
-  export const dynamic = {
+  export const Dynamic = {
     googleMaps: <Location extends string>(
       location: Location
-    ): ReplaceStringPart<typeof navigation.external.googleMaps, Location> => {
-      return navigation.external.googleMaps.replace(
+    ): GTypes.Utils.ReplaceStringPart<
+      typeof Navigation.External.googleMaps,
+      Location
+    > => {
+      return Navigation.External.googleMaps.replace(
         "{location}",
         location
-      ) as ReplaceStringPart<typeof navigation.external.googleMaps, Location>;
+      ) as GTypes.Utils.ReplaceStringPart<
+        typeof Navigation.External.googleMaps,
+        Location
+      >;
     },
   } as const;
 
-  export const redirects = {
-    existingSessionInAuth: navigation.routes.home,
-    loginSucess: navigation.routes.home,
-    registerSuccess: navigation.routes.home,
+  export const Redirects = {
+    existingSessionInAuth: Navigation.Routes.home,
+    loginSucess: Navigation.Routes.home,
+    registerSuccess: Navigation.Routes.home,
   } as const;
 }
